67679de4e4c5b44b0ce407da6b0d8c4aa57c8455
h-ueno2 <menbou1212@gmail.com>
2020-06-20 19:00:18 +0900

    二乗、三乗のメソッドを追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
+use std::ops::Mul;
+
+/// 引数の二乗を返却します。
+/// 
+/// 
+pub fn squared<T: Mul + Mul<Output = T> + Copy>(num: T) -> T {
+    num * num
+}
+
+/// 引数の三乗を返却します。
+/// 
+/// 
+pub fn cubed<T: Mul + Mul<Output = T> + Copy>(num: T) -> T {
+    squared(num) * num
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn squared_test() {
+        assert_eq!(squared(3), 9);
+    }
+
+    #[test]
+    fn cubed_test() {
+        assert_eq!(cubed(2), 8);
     }
 }
