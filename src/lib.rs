c3bca065758887e278a415f9836f5c95e3fda94f
h-ueno2 <menbou1212@gmail.com>
2020-06-21 16:16:43 +0900

    最大公約数を返却する関数を作成

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,29 @@
 pub mod exp;
 pub mod fraction;
+
+/// 引数`a`と`b`の最大公約数を返却します。
+/// 
+pub fn gratest_common_factor(a: u32, b: u32) -> u32 {
+    let mut big_num = if a >= b { a } else { b };
+    let mut small_num = if a >= b { b } else { a };
+    let mut remainder = big_num % small_num;
+    while remainder != 0 {
+        big_num = small_num;
+        small_num = remainder;
+        remainder = big_num % small_num;
+    }
+    small_num
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gratest_common_factor_test() {
+        let a = 3355;
+        let b = 2379;
+
+        assert_eq!(gratest_common_factor(a, b), 61);
+    }
+}
