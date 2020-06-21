669712cf7b11b6b8f231eb97823e5acf7c9a5182
h-ueno2 <menbou1212@gmail.com>
2020-06-21 17:05:23 +0900

    約分の作成

--- src/fraction.rs
+++ src/fraction.rs
@@ -1,8 +1,35 @@
-struct Fraction {
+use super::*;
+
+pub struct Fraction {
     /// 分子
     numerator: u32,
     /// 分母
     denominator: u32,
+}
+
+impl Fraction {
+    pub fn new(numerator: u32, denominator: u32) -> Self {
+        Fraction {
+            numerator,
+            denominator,
+        }
+    }
+
+    /// 約分して初期化します。
+    pub fn new_reduction(numerator: u32, denominator: u32) -> Self {
+        let common_factor = gratest_common_factor(numerator, denominator);
+        Fraction {
+            numerator: numerator / common_factor,
+            denominator: denominator / common_factor,
+        }
+    }
+
+    /// 約分します
+    pub fn reduction(&mut self) {
+        let common_factor = gratest_common_factor(self.numerator, self.denominator);
+        self.numerator = self.numerator / common_factor;
+        self.denominator = self.denominator / common_factor;
+    }
 }
 
 impl std::fmt::Display for Fraction {
@@ -23,5 +50,21 @@ mod tests {
         };
         let text = format!("{}", fraction);
         assert_eq!(text, "3/4");
+    }
+
+    #[test]
+    fn new_reduction_test() {
+        let fraction = Fraction::new_reduction(5, 10);
+
+        let text = format!("{}", fraction);
+        assert_eq!(text, "1/2");
+    }
+
+    #[test]
+    fn reduction_test() {
+        let mut fraction = Fraction::new(5, 10);
+        fraction.reduction();
+        let text = format!("{}", fraction);
+        assert_eq!(text, "1/2");
     }
 }
