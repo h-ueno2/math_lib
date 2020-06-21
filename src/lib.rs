4d015ed191968d957aea532c8fde30a8b5e0315f
h-ueno2 <menbou1212@gmail.com>
2020-06-21 16:44:24 +0900

    最大公約数を求める関数をリファクタリング

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,12 @@ pub mod fraction;
 /// 引数`a`と`b`の最大公約数を返却します。
 /// 
 pub fn gratest_common_factor(a: u32, b: u32) -> u32 {
-    let mut big_num = if a >= b { a } else { b };
-    let mut small_num = if a >= b { b } else { a };
-    let mut remainder = big_num % small_num;
-    while remainder != 0 {
-        big_num = small_num;
-        small_num = remainder;
-        remainder = big_num % small_num;
+    let big_num = if a >= b { a } else { b };
+    let small_num = if a >= b { b } else { a };
+    match big_num % small_num {
+        i if i != 0 => gratest_common_factor(small_num, i),
+        _ => small_num,
     }
-    small_num
 }
 
 #[cfg(test)]
@@ -20,10 +17,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn gratest_common_factor_test() {
-        let a = 3355;
-        let b = 2379;
-
-        assert_eq!(gratest_common_factor(a, b), 61);
+    fn gratest_common_factor_test01() {
+        assert_eq!(gratest_common_factor(3355, 2379), 61);
+    }
+    #[test]
+    fn gratest_common_factor_test02() {
+        assert_eq!(gratest_common_factor(2379, 3355), 61);
     }
 }
