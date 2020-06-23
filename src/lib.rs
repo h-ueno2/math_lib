3199c3a0a57a63c64f7d8c29f62bcf11feab7db0
h-ueno2 <menbou1212@gmail.com>
2020-06-23 21:13:57 +0900

    最大公約数を算出する関数を汎用化

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
+extern crate num_traits;
+
 pub mod exp;
 pub mod fraction;
 
+use num_traits::Zero;
+use std::cmp::PartialOrd;
+use std::ops::Rem;
+
 /// 引数`a`と`b`の最大公約数を返却します。
 /// 
-pub fn gratest_common_factor(a: u32, b: u32) -> u32 {
+pub fn gratest_common_factor<T>(a: T, b: T) -> T 
+where T: Zero + PartialOrd + Rem<Output = T> + Copy
+{
     let big_num = if a >= b { a } else { b };
     let small_num = if a >= b { b } else { a };
     match big_num % small_num {
-        i if i != 0 => gratest_common_factor(small_num, i),
+        i if i != T::zero() => gratest_common_factor(small_num, i),
         _ => small_num,
     }
 }
@@ -23,5 +31,12 @@ mod tests {
     #[test]
     fn gratest_common_factor_test02() {
         assert_eq!(gratest_common_factor(2379, 3355), 61);
+    }
+
+    #[test]
+    fn gratest_common_factor_test03() {
+        let a : u128 = 2379;
+        let b : u128 = 3355;
+        assert_eq!(gratest_common_factor(a, b), 61);
     }
 }
